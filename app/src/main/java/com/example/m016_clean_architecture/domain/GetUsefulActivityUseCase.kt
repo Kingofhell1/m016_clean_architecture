3768664c78e44cbc974e3ee19c6dbb9c69aa41b8
Kingofhell1 <chernyakov_99@list.ru>
2024-06-05 19:11:52 +0300

    Update

--- app/src/main/java/com/example/m016_clean_architecture/domain/GetUsefulActivityUseCase.kt
+++ app/src/main/java/com/example/m016_clean_architecture/domain/GetUsefulActivityUseCase.kt
@@ -5,6 +5,7 @@ import com.example.m016_clean_architecture.data.UsefulActivitiesRepository
 
 class GetUsefulActivityUseCase (private val usefulActivitiesRepository: UsefulActivitiesRepository
 ) {
+    //Выполнение бизнес логики отдельно от слоя данных
     suspend fun execute(): UsefulActivity {
         return usefulActivitiesRepository.getUsefulActivity()
     }
